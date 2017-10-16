c7817e57ebc0116142f44b77d4fbff83c7f4cf36
Dennis Nguyen <nguyen.denn@gmail.com>
2017-10-16 01:01:19 -0700

    initial commit

---
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                case Season.Summer:
+                    Console.WriteLine("It is the perfect time to go to the beach");
+                    break;
+                default:
+                    Console.WriteLine("I don't understand that season");
+                    break;
+            }
+        }
+
+    }
+}
+
